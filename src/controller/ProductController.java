1ef31832a446691ee1aea897c79abb459f8800e9
povsoknem <povsoknem@gmail.com>
2024-03-04 07:33:16 +0700

    fixed

--- src/controller/ProductController.java
+++ src/controller/ProductController.java
@@ -145,7 +145,7 @@ public class ProductController implements BoxBorder {
 
     }
     void clickEnter() {
-        System.out.print("⌨️CLICK ANY KEY TO CONTINUE : ");
+        System.out.print("\ng⌨️CLICK ANY KEY TO CONTINUE : ");
         scanner.nextLine();
     }
     private String getValidProductName() {
